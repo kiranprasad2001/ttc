195f19da22216630cc502faeb15a0da017085c03
Kiran Prasad <32983191+kiranprasad2001@users.noreply.github.com>
2025-01-09 00:28:50 -0500

    either im dumb or the accessibility icon is mocking me

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
             stopElement.innerHTML = `
                 <div class="background-image" style="background-image: url('assets/${backgroundImage}');"></div>
                 <div class="content">
-                    <div class="accessibility-icon" style="position: absolute; top: 3px; left: 3px; z-index: 2; background-color: white; padding: 2px; border-radius: 4px;">${accessibilityIcon}</div>
+                    <div class="accessibility-icon" style="position: absolute; top: 1px; left: 1px; z-index: 2; background-color: white; padding: 1px; border-radius: 1px;">${accessibilityIcon}</div>
                     <h4>${stop.stop_name}</h4>
                     <p>${stop.Routes} - ${Math.round(stop.distance || 0)}m</p>
                     <p>${stop.Direction}</p>
